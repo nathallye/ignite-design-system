aad9148a19b3c5c6bf94efcf9c9f92afbebe011d
Nathallye Bacelar <nathallyet@gmail.com>
2023-07-01 01:14:40 -0300

    Heading component

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Text, TextProps } from "@ignite-ui/react";
 
 export default {
-  title: "Tyography/Text",
+  title: "Typography/Text",
   component: Text,
   args: {
     children: "Example text"
